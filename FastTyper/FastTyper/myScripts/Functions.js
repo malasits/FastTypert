6699a1a9003f440c7f6dbddd6378c4f3ef99baaa
malasits <malasits@hotmail.com>
2017-11-19 13:42:39 +0100

    Write functions
    Characters, SelectChatacter()

--- FastTyper/FastTyper/myScripts/Functions.js
+++ FastTyper/FastTyper/myScripts/Functions.js
@@ -1,1 +1,9 @@
-﻿
+﻿var Characters = [
+    '1', '2', '3', '4', '5', '6', '7', '8', '9', 'ö', 'ü', 'ó', 'q', 'w', 'e', 'r', 't', 'z', 'u', 'i', 'o', 'p', 'ő', 'ú', 'a', 's', 'd', 'f', 'g',
+    'h', 'j', 'k', 'l', 'é', 'á', 'ű', 'y', 'x', 'c', 'v', 'b', 'n', 'm', 'Ö', 'Ü', 'Ó', 'Q', 'W', 'E', 'R', 'T', 'Z', 'U', 'I', 'O', 'P', 'Ő', 'Ú',
+    'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'É', 'Á', 'Ű', 'Y', 'X', 'C', 'V', 'B', 'N', 'M', ]; //77 character
+
+function SelectCharacter() {
+    var rnd = Math.floor(Math.random() * Characters.length);
+    document.getElementById("RandomChar").innerHTML = Characters[rnd];
+}
