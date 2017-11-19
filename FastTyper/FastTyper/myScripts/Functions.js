a3cd0748692dcb5cd69bdcfa2a1c12102580aa15
malasits <malasits@hotmail.com>
2017-11-19 16:36:21 +0100

    Timer
    Timer is active, and it has got minimal style

--- FastTyper/FastTyper/myScripts/Functions.js
+++ FastTyper/FastTyper/myScripts/Functions.js
@@ -3,6 +3,8 @@
     'h', 'j', 'k', 'l', 'é', 'á', 'ű', 'y', 'x', 'c', 'v', 'b', 'n', 'm', 'Ö', 'Ü', 'Ó', 'Q', 'W', 'E', 'R', 'T', 'Z', 'U', 'I', 'O', 'P', 'Ő', 'Ú',
     'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'É', 'Á', 'Ű', 'Y', 'X', 'C', 'V', 'B', 'N', 'M', ]; //77 character
 
+var Start_time = 55;
+
 function SelectCharacter() {
     var rnd = Math.floor(Math.random() * Characters.length);
     document.getElementById("RandomChar").innerHTML = Characters[rnd];
@@ -17,4 +19,13 @@ function Keyboard() {
         }
     }
     document.getElementById("Keyboard").innerHTML = insertkeyboard;
+}
+
+function Timer() {
+    var min = Math.floor(Start_time / 60);
+    var sec = Start_time - (60 * min);
+
+    document.getElementById("Timer").innerHTML = min + " : " + sec;
+
+    Start_time += 1;
 }
