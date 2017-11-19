95a03d901f20a995ee0efa5c708ade256d90c158
malasits <malasits@hotmail.com>
2017-11-19 18:34:55 +0100

    Dog

--- FastTyper/FastTyper/myScripts/Functions.js
+++ FastTyper/FastTyper/myScripts/Functions.js
@@ -1,9 +1,11 @@
 ﻿var Characters = [
-    '1', '2', '3', '4', '5', '6', '7', '8', '9', 'ö', 'ü', 'ó', 'q', 'w', 'e', 'r', 't', 'z', 'u', 'i', 'o', 'p', 'ő', 'ú', 'a', 's', 'd', 'f', 'g',
-    'h', 'j', 'k', 'l', 'é', 'á', 'ű', 'y', 'x', 'c', 'v', 'b', 'n', 'm', 'Ö', 'Ü', 'Ó', 'Q', 'W', 'E', 'R', 'T', 'Z', 'U', 'I', 'O', 'P', 'Ő', 'Ú',
-    'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'É', 'Á', 'Ű', 'Y', 'X', 'C', 'V', 'B', 'N', 'M', ]; //77 character
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'ö', 'ü', 'ó', 'q', 'w', 'e', 'r', 't', 'z', 'u', 'i', 'o', 'p', 'ő', 'ú', 'a', 's', 'd', 'f',
+    'g', 'h', 'j', 'k', 'l', 'é', 'á', 'ű', 'y', 'x', 'c', 'v', 'b', 'n', 'm', 'Ö', 'Ü', 'Ó', 'Q', 'W', 'E', 'R', 'T', 'Z', 'U', 'I', 'O', 'P', 'Ő',
+    'Ú', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'É', 'Á', 'Ű', 'Y', 'X', 'C', 'V', 'B', 'N', 'M', ]; //78 character
 
-var Start_time = 55;
+var Start_time = 0;
+var CorrectWords = 0;
+var WrongWords = 0;
 
 function SelectCharacter() {
     var rnd = Math.floor(Math.random() * Characters.length);
@@ -14,8 +16,13 @@ function Keyboard() {
     var insertkeyboard = "";
 
     if(1 == 1){
-        for (var i = 9; i < 43; i++) {
-            insertkeyboard += "<div>" + Characters[i] + "</div>";
+        for (var i = 0; i < 44; i++) {
+            if (i == 12) {
+                insertkeyboard += "<div class='ButtonsNewLine' id ='btn" + i + "'>" + Characters[i] + "</div>";
+            }
+            else {
+                insertkeyboard += "<div class='Buttons' id ='btn" + i + "'>" + Characters[i] + "</div>";
+            }           
         }
     }
     document.getElementById("Keyboard").innerHTML = insertkeyboard;
